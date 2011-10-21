c9614cd36756513e94cc817a9795e5fbdeac9bec
Manan Shah <kewlmanan@hotmail.com>
2011-10-20 21:09:09 -0700

    Making it more readable

--- m.pl
+++ m.pl
@@ -1,5 +1,11 @@
 #! /usr/sbin/perl 
 
+# Born out of my laziness!
+# This script will make a new .pl file for you with basic headers in it 
+# and also give it  the appropriate permission for it to run !
+
+# USAGE : perl m.pl filename
+
 use strict;
 
 if( $#ARGV != 0)
@@ -13,10 +19,13 @@ if ( -e $ARGV[0])
 	die " File already exists \n";
 }
 
-open(handler,">$ARGV[0]");
+my $filename=$ARGV[0].".pl";
+
+open(handler,">$filename");
 
 print handler "#! /usr/bin/perl \n";
 print handler "use strict;";
+print handler "use warnings;";
 
 close (handler);
-`chmod +x $ARGV[0]`; 
+`chmod +x $filename`; 
